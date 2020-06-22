3896a8b5fc8c054acf877a5e2f94a1aaa6a98168
brunocheble <contato.cheble@gmail.com>
2020-06-22 01:06:35 +0100

    Atualização no ORMconfig para aceitar url do mongodb

--- ormconfig.js
+++ ormconfig.js
@@ -1,9 +1,10 @@
 module.exports = {
   "name": "default",
   "type": "mongodb",
-  "host": process.env.TYPEORM_HOST,
-  "port": process.env.TYPEORM_PORT,
-  "database": process.env.TYPEORM_DATABASE,
+  "url": process.env.TYPEORM_HOST,
+  "useNewUrlParser": true,
+  "synchronize": true,
+  "logging": true,
   "useUnifiedTopology": true,
   "entities": [
     "./src/modules/**/infra/typeorm/schemas/*.ts"
